9fec4023b7eb3f8c88d8459d3474839fe0961f3e
tom <tom@sparky>
2013-11-15 21:22:20 +0000

    Add test for object serialisation/deserialisation

--- unittests.py
+++ unittests.py
@@ -2,6 +2,24 @@ import unittest
 import os
 import utils
 import data_loader
+import tempfile
+
+class TestUtilsFunctions(unittest.TestCase):
+
+    def test_serialise_deserialise_obj(self):
+
+        data = {
+                'a': [5.97, 2.97, 8.2502, 4],
+                'b': 'hello',
+                'c': True
+                }
+        fd, tmpfile = tempfile.mkstemp(suffix='.py')
+        try:
+            utils.serialise_obj(data, tmpfile)
+            result = utils.deserialise_obj(tmpfile)
+        finally:
+            os.remove(tmpfile)
+        self.assertEqual(data, result)
 
 class TestDataLoaderFunctions(unittest.TestCase):
 
