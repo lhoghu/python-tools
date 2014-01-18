5f2b49df5d1da4ff0b79e4a51ed6d25fc712d984
BlueTrin <anthony@nguyen.ly>
2014-01-18 19:16:52 +0000

    Added config files for global variables cherry picked 6739406

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import ConfigParser
+import os
+
 CACHE_FOLDER = 'cache'
 CSV_FOLDER = 'csv'
 
@@ -7,3 +10,18 @@ MONGO_FOLDER = 'mongo'
 MONGOD_PORT = 27021
 MONGO_LOG = 'mongo.log'
 MONGO_TIMESERIES_DB = 'timeseries_db'
+
+def main():
+    config = ConfigParser.ConfigParser()
+    config.readfp(open('defaults.cfg'))
+    config.read(['site.cfg', os.path.expanduser('~/.python-tools.cfg')])
+    CACHE_FOLDER = config.get('folders', 'cache_folder')
+    CSV_FOLDER = config.get('folders', 'csv_folder')
+    DB = config.get('database', 'db')
+    MONGO_FOLDER = config.get('mongo', 'mongo_folder')
+    MONGOD_PORT = config.get('mongo', 'mongod_port')
+    MONGO_LOG = config.get('mongo', 'mongod_log')
+    MONGO_TIMESERIES_DB = config.get('mongo', 'mongo_timeseries_db')
+
+if __name__ == '__main__':
+    main()
