796c13e674194f51440ab3dce4ecb0d3b46be8e3
BlueTrin <anthony@nguyen.ly>
2014-01-26 19:39:32 +0000

    Data downloading will not log more information, which will be useful in the case of failure

--- test_fetchequitybbg.py
+++ test_fetchequitybbg.py
@@ -40,11 +40,12 @@ def convert_bbg_indices(index_list, start_date, end_date):
     equ_set = set()
 
     # Get the time series from the cache
-    logging.basicConfig(filename='debug.log', level=logging.DEBUG)
     equity_set = set()
     symbol_to_idx = dict()
     idx_to_symbol = list()
     for index in index_list:
+        logging.info('working on index composition: ' + index)
+
         field = ''
         if index == 'DJI Index':
             field = 'INDX_MWEIGHT'
@@ -87,11 +88,11 @@ def get_bbg_indices(index_list, start_date, end_date):
     equ_set = set()
 
     # Get the time series from the cache
-    logging.basicConfig(filename='debug.log', level=logging.DEBUG)
     equity_set = set()
     symbol_to_idx = dict()
     idx_to_symbol = list()
     for index in index_list:
+        logging.info('working on index <' + index + '>')
         field = ''
         if index == 'DJI Index':
             field = 'INDX_MWEIGHT'
@@ -107,13 +108,10 @@ def get_bbg_indices(index_list, start_date, end_date):
             }
 
         ts = data_retrieval.get_time_series(loader, loader_args)
-        for date, equ_tup in ts:
-            equ_set.update(equ_tup)
-#        if ts is not None:
-#            total_ts = loader_args.copy()
-#            total_ts['ts'] = ts
-#            print "WRITING " + index
-#            data_retrieval.write_time_series(total_ts)
+        logging.info('index <' + index + '>: timeseries found')
+        if ts:
+            for date, equ_tup in ts:
+                equ_set.update(equ_tup)
 
 
     equ_list = list(equ_set);
@@ -161,6 +159,8 @@ def get_bbg_equdata(equ_list, start_date, end_date):
 
     for equity in equ_list:
         equity += " Equity"
+        logging.info('working on equity <' + equity + '>')
+
         for field in fields:
             loader = 'download_bbg_timeseries';
             loader_args = {
@@ -171,18 +171,22 @@ def get_bbg_equdata(equ_list, start_date, end_date):
                 }
 
             ts = data_retrieval.get_time_series(loader, loader_args)
+            if ts:
+                logging.info('  <' + equity + '><' + field + '>:' + str(len(ts)) + ' records')
+            else:
+                logging.info('  <' + equity + '><' + field + '>: failed to retrieve')
 
 
-#            if ts is not None:
-#                print "WRITING " + equity
-#                total_ts = loader_args.copy()
-#                total_ts['ts'] = ts
-#                config.DB = data_retrieval.MONGO_DB
-#                data_retrieval.write_time_series(total_ts)
+
 
 ################################################################################
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='../../%s-log.txt' % datetime.datetime.strftime(datetime.datetime.now(),"%Y%m%d %H%M%S"),level=logging.DEBUG)
+    logging.info('Starting operation')
+    import utils
+    utils.s_deserialise_obj('../cache/d5d8370a0b7b9c67f362ea0f7456b9747ef9121a.spickle')
+
     t = datetime.datetime(2014, 1, 4)
     today_date = datetime.datetime(t.year, t.month, t.day)
     start_date = datetime.datetime(1990, 1, 1)
@@ -191,14 +195,18 @@ if __name__ == '__main__':
     config.DB = "cache"
     config.SERIALISER = "spickle"
 
-    index_list = ('SPX Index', 'DJI Index', 'RTY Index')
-    test_spickle_vs_csv(index_list, start_date, end_date)
+    #index_list = ('SPX Index', 'DJI Index', 'RTY Index')
+    #test_spickle_vs_csv(index_list, start_date, end_date)
     #convert_bbg_indices(index_list, start_date, end_date)
 
     # index_list = ('SPX Index',)
-    # index_list = ('SPX Index', 'DJI Index', 'RTY Index')
-    # index_list = ('SPX Index', 'DJI Index', 'RTY Index', 'RAY Index')
-    # equ_list = get_bbg_indices(index_list, start_date, end_date)
+    # RTY = Russell 2000
+    # RIY = Russell 1000
+    # RAY = Russell 3000
 
-    # start_date = datetime.datetime(1960, 1, 1)
-    # get_bbg_equdata(equ_list, start_date, end_date)
+    #index_list = ('SPX Index',)
+    index_list = ('SPX Index', 'RTY Index', 'RIY Index')
+    equ_list = get_bbg_indices(index_list, start_date, end_date)
+
+    equ_start_date = datetime.datetime(1960, 1, 1)
+    get_bbg_equdata(equ_list, equ_start_date, end_date)
